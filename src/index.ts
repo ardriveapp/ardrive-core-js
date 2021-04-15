33050382fb2cbfabea885135089d1466637e752b
Philip Mataras <vilenarios@gmail.com>
2021-04-14 21:01:31 -0400

    Changed to * for all exports

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,9 @@ export * from './files';
 export * from './upload';
 export * from './profile';
 export * from './types';
-export { getWalletBalance, getLocalWallet, createArDriveWallet } from './arweave';
-export { getAllMyPrivateArDriveIds, getAllMyPublicArDriveIds } from './gql';
-export { setupDatabase } from './db';
-export {
-	getUserFromProfileById,
-	getUserFromProfile,
-	getMyFileDownloadConflicts,
-	getDriveFromDriveTable,
-	getAllDrivesByLoginFromDriveTable,
-	getAllUnSyncedPersonalDrivesByLoginFromDriveTable,
-	getProfileWalletBalance
-} from './db_get';
-export { setProfileWalletBalance, setDriveToSync, addDriveToDriveTable, setProfileAutoSyncApproval } from './db_update';
+export * from './arweave';
+export * from './gql';
+export * from './crypto';
+export * from './db';
+export * from './db_get';
+export * from './db_update';
