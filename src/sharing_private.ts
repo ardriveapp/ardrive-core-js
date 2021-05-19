c48428cdfb5f297582a93bd3495b2ab77a832804
jdaev <jdhn42@gmail.com>
2021-05-19 09:32:57 -0400

    refactor: Change createArfsPrivateFileSharingLink arguments

--- src/sharing_private.ts
+++ src/sharing_private.ts
@@ -1,24 +1,12 @@
-import { ArDriveUser } from './types/base_Types';
-import { ArFSLocalFile } from './types/client_Types';
-import { deriveDriveKey, deriveFileKey } from './crypto';
+import { deriveFileKey } from './crypto';
 import { stagingAppUrl } from './constants';
 
 // Derives a file key from the drive key and formats it into a Private file sharing link using the file id
-export async function createArFSPrivateFileSharingLink(user: ArDriveUser, fileToShare: ArFSLocalFile): Promise<string> {
+export async function createArFSPrivateFileSharingLink(driveKey: Buffer, fileId: string): Promise<string> {
 	let fileSharingUrl = '';
 	try {
-		const driveKey: Buffer = await deriveDriveKey(
-			user.dataProtectionKey,
-			fileToShare.entity.driveId,
-			user.walletPrivateKey
-		);
-		const fileKey: Buffer = await deriveFileKey(fileToShare.entity.entityId, driveKey);
-		fileSharingUrl = stagingAppUrl.concat(
-			'/#/file/',
-			fileToShare.entity.entityId,
-			'/view?fileKey=',
-			fileKey.toString('base64')
-		);
+		const fileKey: Buffer = await deriveFileKey(fileId, driveKey);
+		fileSharingUrl = stagingAppUrl.concat('/#/file/', fileId, '/view?fileKey=', fileKey.toString('base64'));
 	} catch (err) {
 		console.log(err);
 		console.log('Cannot generate Private File Sharing Link');
