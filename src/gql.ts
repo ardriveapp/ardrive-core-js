f6065c38668e433c66d650b76dbe2b71248a7bf5
jdaev <jdhn42@gmail.com>
2021-05-29 20:43:06 -0400

    task: Move getFileMetaDataFromTx to gql.ts

--- src/gql.ts
+++ src/gql.ts
@@ -2,8 +2,12 @@ import * as common from './common';
 import * as types from './types/base_Types';
 import * as arfsTypes from './types/arfs_Types';
 import * as gqlTypes from './types/gql_Types';
+import * as getDb from './db/db_get';
+import * as updateDb from './db/db_update';
+
 import { getTransactionData } from './gateway';
-import { deriveDriveKey, driveDecrypt } from './crypto';
+import { deriveDriveKey, driveDecrypt, deriveFileKey, fileDecrypt } from './crypto';
+
 import Arweave from 'arweave';
 
 const arweave = Arweave.init({
@@ -2280,7 +2284,174 @@ export async function getAllMySharedDataFileTxs(
 	}
 	return edges;
 }
+// Takes an ArDrive File Data Transaction and writes to the database.
+export async function getFileMetaDataFromTx(fileDataTx: gqlTypes.GQLEdgeInterface, user: types.ArDriveUser) {
+	const fileToSync: types.ArFSFileMetaData = types.ArFSFileMetaData.Empty(user.login);
+	try {
+		const { node } = fileDataTx;
+		const { tags } = node;
+		fileToSync.metaDataTxId = node.id;
 
+		// DOUBLE CHECK THIS
+		// Is the File or Folder already present in the database?  If it is, lets ensure its already downloaded
+		const isMetaDataSynced = await getDb.getByMetaDataTxFromSyncTable(fileToSync.metaDataTxId);
+		if (isMetaDataSynced) {
+			// this file is already downloaded and synced
+			return 'Synced Already';
+		}
+
+		// Download the File's Metadata using the metadata transaction ID
+		const data: string | Uint8Array = await getTransactionData(fileToSync.metaDataTxId);
+
+		// Enumerate through each tag to pull the data
+		tags.forEach((tag: gqlTypes.GQLTagInterface) => {
+			const key = tag.name;
+			const { value } = tag;
+			switch (key) {
+				case 'App-Name':
+					fileToSync.appName = value;
+					break;
+				case 'App-Version':
+					fileToSync.appVersion = value;
+					break;
+				case 'Unix-Time':
+					fileToSync.unixTime = +value; // Convert to number
+					break;
+				case 'Content-Type':
+					fileToSync.contentType = value;
+					break;
+				case 'Entity-Type':
+					fileToSync.entityType = value;
+					break;
+				case 'Drive-Id':
+					fileToSync.driveId = value;
+					break;
+				case 'File-Id':
+					fileToSync.fileId = value;
+					break;
+				case 'Folder-Id':
+					fileToSync.fileId = value;
+					break;
+				case 'Parent-Folder-Id':
+					fileToSync.parentFolderId = value;
+					break;
+				case 'Cipher':
+					fileToSync.cipher = value;
+					break;
+				case 'Cipher-IV':
+					fileToSync.metaDataCipherIV = value;
+					break;
+				default:
+					break;
+			}
+		});
+
+		let dataJSON;
+		let decryptedData = Buffer.from('');
+		// If it is a private file or folder, the data will need decryption.
+		if (fileToSync.cipher === 'AES256-GCM') {
+			fileToSync.isPublic = 0;
+			const dataBuffer = Buffer.from(data);
+			const driveKey: Buffer = await deriveDriveKey(
+				user.dataProtectionKey,
+				fileToSync.driveId,
+				user.walletPrivateKey
+			);
+			if (fileToSync.entityType === 'file') {
+				// Decrypt files using a File Key derived from the Drive key
+				const fileKey: Buffer = await deriveFileKey(fileToSync.fileId, driveKey);
+				decryptedData = await fileDecrypt(fileToSync.metaDataCipherIV, fileKey, dataBuffer);
+			} else if (fileToSync.entityType === 'folder') {
+				// Decrypt folders using the Drive Key only
+				decryptedData = await fileDecrypt(fileToSync.metaDataCipherIV, driveKey, dataBuffer);
+			}
+
+			// Handle an error with decryption by ignoring this file.  THIS NEEDS TO BE IMPROVED.
+			if (decryptedData.toString('ascii') === 'Error') {
+				console.log(
+					'There was a problem decrypting a private %s with TXID: %s',
+					fileToSync.entityType,
+					fileToSync.metaDataTxId
+				);
+				console.log('Skipping this file...');
+				fileToSync.fileSize = 0;
+				fileToSync.fileName = '';
+				fileToSync.fileHash = '';
+				fileToSync.fileDataSyncStatus = 0;
+				fileToSync.fileMetaDataSyncStatus = 3;
+				fileToSync.dataTxId = '0';
+				fileToSync.lastModifiedDate = fileToSync.unixTime;
+				fileToSync.permaWebLink = common.gatewayURL.concat(fileToSync.dataTxId);
+				fileToSync.cloudOnly = 1;
+				await updateDb.addFileToSyncTable(fileToSync); // This must be handled better.
+				return 'Error Decrypting';
+			} else {
+				const dataString = await common.Utf8ArrayToStr(decryptedData);
+				dataJSON = await JSON.parse(dataString);
+			}
+		} else {
+			// the file is public and does not require decryption
+			const dataString = await common.Utf8ArrayToStr(data);
+			dataJSON = await JSON.parse(dataString);
+			fileToSync.isPublic = 1;
+		}
+
+		// Set metadata for Folder and File entities
+		fileToSync.fileSize = dataJSON.size;
+		fileToSync.fileName = dataJSON.name;
+		fileToSync.fileHash = '';
+		fileToSync.fileDataSyncStatus = 3;
+		fileToSync.fileMetaDataSyncStatus = 3;
+		fileToSync.dataTxId = '0';
+
+		// Perform specific actions for File, Folder and Drive entities
+		if (fileToSync.entityType === 'file') {
+			// The actual data transaction ID, lastModifiedDate, and Filename of the underlying file are pulled from the metadata transaction
+			fileToSync.lastModifiedDate = dataJSON.lastModifiedDate; // Convert to milliseconds
+			fileToSync.dataTxId = dataJSON.dataTxId;
+			fileToSync.contentType = common.extToMime(dataJSON.name);
+			fileToSync.permaWebLink = common.gatewayURL.concat(dataJSON.dataTxId);
+
+			if (fileToSync.isPublic === 0) {
+				// if this is a private file, the CipherIV of the Data transaction should also be captured
+				fileToSync.dataCipherIV = await getPrivateTransactionCipherIV(fileToSync.dataTxId);
+			}
+
+			// Check to see if a previous version exists, and if so, increment the version.
+			// Versions are determined by comparing old/new file hash.
+			const latestFile = await getDb.getLatestFileVersionFromSyncTable(fileToSync.fileId);
+			if (latestFile !== undefined) {
+				if (latestFile.fileDataTx !== fileToSync.dataTxId) {
+					fileToSync.fileVersion = +latestFile.fileVersion + 1;
+					// console.log ("%s has a new version %s", dataJSON.name, fileToSync.fileVersion)
+				}
+				// If the previous file data tx matches, then we do not increment the version
+				else {
+					fileToSync.fileVersion = latestFile.fileVersion;
+				}
+			}
+			// Perform specific actions for Folder entities
+		} else if (fileToSync.entityType === 'folder') {
+			fileToSync.lastModifiedDate = fileToSync.unixTime;
+			fileToSync.permaWebLink = common.gatewayURL.concat(fileToSync.metaDataTxId);
+		}
+
+		console.log(
+			'QUEUING %s %s | Id: %s | Tx: %s for download',
+			fileToSync.entityType,
+			fileToSync.fileName,
+			fileToSync.fileId,
+			fileToSync.metaDataTxId
+		);
+		await updateDb.addFileToSyncTable(fileToSync);
+		return 'Success';
+	} catch (err) {
+		console.log(err);
+		console.log('Error syncing file metadata');
+		console.log(fileToSync);
+		return 'Error syncing file metadata';
+	}
+}
 function tagToAttributeMap(tag: string): string {
 	// tag to camel case
 	const words = tag.split('-');
