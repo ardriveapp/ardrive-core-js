ee5fa27693499ad9e79a65748f44816423a0e0aa
Mati <matibat@gmail.com>
2021-06-08 16:38:43 -0300

    Finishes the refactor of all gql methods

--- src/gql/dataQueries.ts
+++ src/gql/dataQueries.ts
@@ -1,6 +1,9 @@
+import { appName, webAppName } from '../common';
 import { ArFSFileData, IFileData } from '../types/arfs_Types';
+import { GQLEdgeInterface } from '../types/gql_Types';
 import { PrivacyToData } from '../types/type_conditionals';
 import { drivePrivacy } from '../types/type_guards';
+import { NODE_ID_AND_TAGS_PARAMETERS, Query } from './Query';
 import { getTransactionQuery } from './transactionQueries';
 
 export const getPublicFileEntity = getPrivateFileEntityFactory<drivePrivacy.PRIVATE>();
@@ -19,3 +22,35 @@ async function getFileData<P extends drivePrivacy>(txId: string): Promise<Privac
 	const entityInstance = new ArFSFileData<P>(entities[0]) as PrivacyToData<P>;
 	return entityInstance;
 }
+
+export async function getAllMyDataFileTxs(
+	walletPublicKey: string | false,
+	driveId: string,
+	lastBlockHeight: number
+): Promise<GQLEdgeInterface[]> {
+	// Search last 5 blocks minimum
+	if (lastBlockHeight > 5) {
+		lastBlockHeight -= 5;
+	}
+	const query = new Query();
+	query.lastDriveBlockHeight = lastBlockHeight;
+	if (walletPublicKey) {
+		query.owners = [walletPublicKey];
+	}
+	query.tags = [
+		{ name: 'App-Name', values: [appName, webAppName] },
+		{ name: 'Drive-Id', values: driveId },
+		{ name: 'Entity-Type', values: ['file', 'folder'] }
+	];
+	query.first = 100;
+	query.parameters = [
+		'pageInfo.hasNextPage',
+		'edges.cursor',
+		'edges.node.block.timestamp',
+		'edges.node.block.height',
+		...NODE_ID_AND_TAGS_PARAMETERS
+	];
+	return await query.getRaw();
+}
+
+export const getAllMySharedDataFileTxs = getAllMyDataFileTxs.bind(this, false);
--- src/gql/index.ts
+++ src/gql/index.ts
@@ -3,6 +3,3 @@ export * from './folderQueries';
 export * from './fileQueries';
 export * from './transactionQueries';
 export * from './metaDataQueries';
-
-// TODO: delete when completely replaced:
-export * from './gql';
