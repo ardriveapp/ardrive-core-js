53ead3bf5b93290f490b00f045a6a12a8cd27be0
Mati <matibat@gmail.com>
2021-06-10 20:14:51 -0300

    feat(guards): Replaces nested guards definitions by enums with explicit values

--- src/types/type_guards.ts
+++ src/types/type_guards.ts
@@ -1,5 +1,35 @@
-export type cipherType = 'aes-gcm-256' | 'AES256-GCM' | 'invalid' | string;
-export type entityType = 'drive' | 'file' | 'folder' | 'invalid' | string;
-export type drivePrivacy = 'private' | 'public' | 'invalid' | string;
-export type driveAuthMode = 'password' | 'invalid' | string;
-export type syncStatus = 0 | 1 | 2 | 3;
+export type emptyString = '';
+export const emptyString: emptyString = '';
+export const AES256_GCM = 'AES256-GCM';
+export type cipherType = 'aes-gcm-256' | 'AES256-GCM';
+export enum entityType {
+	DRIVE = 'drive',
+	FILE = 'file',
+	FOLDER = 'folder',
+	EMPTY = ''
+}
+export enum contentType {
+	APPLICATION_JSON = 'application/json',
+	APPLICATION_OCTET_STREAM = 'application/octet-stream',
+	EMPTY = ''
+}
+export enum drivePrivacy {
+	PRIVATE = 'private',
+	PUBLIC = 'public',
+	EMPTY = ''
+}
+export type driveAuthMode = 'password';
+export enum driveSharing {
+	SHARED = 'shared',
+	PERSONAL = 'personal'
+}
+export enum syncStatus {
+	READY_TO_DOWNLOAD = 0,
+	READY_TO_UPLOAD = 1,
+	GETTING_MINED = 2,
+	SUCCESSFULLY_UPLOADED = 3
+}
+export enum yesNoInteger {
+	NO = 0,
+	YES = 1
+}
