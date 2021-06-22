8c6a5e1cff0328b7271c2d62aca192361846509f
Derek Sonnenberg <derek@ardrive.io>
2021-06-22 11:17:02 -0500

    test(setupLibraries): Add config/setup file for sinon PE-3

--- .mocharc.js
+++ .mocharc.js
@@ -5,7 +5,7 @@
 module.exports = {
 	extension: ['ts'],
 	spec: ['src/**/*.test.ts', 'test/**/*.test.ts'],
-	require: ['ts-node/register/transpile-only', 'source-map-support/register'],
+	require: ['ts-node/register/transpile-only', 'source-map-support/register', 'test/testSetup.ts'],
 	timeout: '3000',
 	parallel: true,
 	recursive: true
