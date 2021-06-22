c685106e32c68bdc05f36776c5486fd0ebc22e44
Derek Sonnenberg <derek@ardrive.io>
2021-06-22 13:55:51 -0500

    test(setupLibraries): Add `yarn coverage` command
    This will produce a verbose terminal output with coverage details
    
    PE-3

--- nyc.config.js
+++ nyc.config.js
@@ -1,17 +1,14 @@
 'use-strict';
 
 // Istanbul `nyc` configuration file
-// Reference for options: https://github.com/istanbuljs/nyc#common-configuration-options
+// Reference for config options: https://github.com/istanbuljs/nyc#common-configuration-options
 module.exports = {
-	extension: ['.ts', '.tsx'],
+	extension: ['.ts'],
 	exclude: ['**/*.d.ts', 'src/**/*.test.ts'],
 	include: 'src/**/*.ts',
-	reporter: [
-		// Text reporter adds robust coverage terminal output to each yarn test
-		'text',
-		// HTML reporter produces HTML files found in `/coverage`
-		'html'
-	]
+	// Reporter options: https://istanbul.js.org/docs/advanced/alternative-reporters/
+	reporter: ['text-summary', 'html']
+
 	// Coverage options (once we have some coverage)
 	// 'check-coverage': true,
 	// branches: 80,
