b9c4bbfcba61db2484b4192c7b5841b39fd5d044
Mati <matibat@gmail.com>
2021-06-26 19:01:45 -0300

    draft(types): Adds a type conditional for typeing intantiable entitites PE-12

--- src/types/type_conditionals.ts
+++ src/types/type_conditionals.ts
@@ -1,18 +1,22 @@
 import {
 	ArFSDriveEntity,
+	ArFSEntity,
 	ArFSFileFolderEntity,
 	ArFSPrivateDriveEntity,
 	ArFSPrivateFileData,
 	ArFSPrivateFileFolderEntity,
 	ArFSPublicDriveEntity,
 	ArFSPublicFileData,
-	ArFSPublicFileFolderEntity
+	ArFSPublicFileFolderEntity,
+	IEntity
 } from './arfs_Types';
 import { DrivePrivacy, PrivateType } from './type_guards';
 
 export type Instantiable<T, A> = {
 	new (a?: A): T;
 };
+
+export type InstantiableEntity<T extends IEntity = IEntity> = Instantiable<ArFSEntity<T>, T>;
 
 export type PrivacyToDriveEntity<P extends DrivePrivacy> = ArFSDriveEntity<
 	P extends PrivateType ? ArFSPrivateDriveEntity : ArFSPublicDriveEntity
