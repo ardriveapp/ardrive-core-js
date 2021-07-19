0a2e86eb9fd13eed74e9eb8f2d0f568a0a006a6b
Derek Sonnenberg <derek@ardrive.io>
2021-07-19 17:16:15 -0500

    refactor(feeDistribution): Send background task after startup PE-50

--- src/public/ardrive_community_oracle.ts
+++ src/public/ardrive_community_oracle.ts
@@ -9,8 +9,6 @@ let arDriveTipPercentage: number | undefined = undefined;
 // Exact block height of the community vote that set the ArDrive Community Fee
 const communityTipBlockHeight = 578358;
 
-let isTipPercentageLatestFromSmartWeave = false;
-let isFetchingTipSetting = false;
 export class ArDriveCommunityOracle implements CommunityOracle {
 	async getCommunityARTip(arCost: number): Promise<number> {
 		const arDriveCommunityTip = arCost * (await this.getArDriveTipPercentage());
@@ -19,18 +17,13 @@ export class ArDriveCommunityOracle implements CommunityOracle {
 
 	// Calls the ArDrive Community Smart Contract to pull the tip percentage
 	async getArDriveTipPercentage(contractOracle: ContractOracle = new SmartWeaveContractOracle()): Promise<number> {
-		if (!isTipPercentageLatestFromSmartWeave) {
-			// If tip was not gathered from exact setting, start background task to gather, do not await
-			this.setExactTipSettingInBackground();
-		}
-
 		if (arDriveTipPercentage !== undefined) {
 			// Tip percentage has already been fetched, return the value
 			return arDriveTipPercentage;
 		}
 
 		try {
-			// Tip has not been calculated, read from contract (4-6 seconds)
+			// Tip has not been calculated, use block height to read from contract (4-6 seconds)
 			const communityTipValue = await contractOracle.getTipSetting(communityTipBlockHeight);
 
 			if (communityTipValue) {
@@ -58,14 +51,6 @@ export class ArDriveCommunityOracle implements CommunityOracle {
 	async setExactTipSettingInBackground(
 		contractOracle: ContractOracle = new SmartWeaveContractOracle()
 	): Promise<number> {
-		if ((isTipPercentageLatestFromSmartWeave || isFetchingTipSetting) && process.env.NODE_ENV !== 'test') {
-			// Exact tip percentage has already been derived or
-			// the contract is currently being read, return early
-			return 0;
-		}
-
-		isFetchingTipSetting = true;
-
 		try {
 			// Reads full smart contract, returns community tip value (35-50 seconds)
 			const arDriveCommTipFromSettings = await contractOracle.getTipSetting();
@@ -73,7 +58,6 @@ export class ArDriveCommunityOracle implements CommunityOracle {
 			if (arDriveCommTipFromSettings) {
 				// Exact tip percentage has been retrieved from settings, set in cache
 				arDriveTipPercentage = arDriveCommTipFromSettings / 100;
-				isTipPercentageLatestFromSmartWeave = true;
 				return arDriveTipPercentage;
 			} else {
 				return 0;
@@ -81,8 +65,13 @@ export class ArDriveCommunityOracle implements CommunityOracle {
 		} catch (err) {
 			console.log(err);
 			return 0;
-		} finally {
-			isFetchingTipSetting = false;
 		}
 	}
+}
+
+if (process.env.NODE_ENV !== 'test') {
+	// Sends background task after initial startup, but not during testing
+	setTimeout(() => {
+		new ArDriveCommunityOracle().setExactTipSettingInBackground();
+	}, 500);
 }
