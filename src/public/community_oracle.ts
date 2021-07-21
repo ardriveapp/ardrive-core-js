b88b8f63b563179dbf95c067ce373350f91ad68e
Derek Sonnenberg <derek@ardrive.io>
2021-07-21 10:04:25 -0500

    refactor(feeDistribution): Move `ContractOracle` to its own file PE-50

--- src/public/community_oracle.ts
+++ src/public/community_oracle.ts
@@ -1,7 +1,3 @@
 export interface CommunityOracle {
 	getCommunityARTip(arCost: number): Promise<number>;
 }
-
-export interface ContractOracle {
-	getTipSetting(height?: number): Promise<number>;
-}
