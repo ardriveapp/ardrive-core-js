59beb369539a57f7f150308a68a0903c9a231360
Mati <matibat@gmail.com>
2021-11-22 19:44:44 -0300

    test(StreamDecrypt): implement the pending tests PE-461

--- src/utils/stream_decrypt.test.ts
+++ src/utils/stream_decrypt.test.ts
@@ -1,4 +1,40 @@
+import { expect } from 'chai';
+import { Readable, pipeline } from 'stream';
+import { promisify } from 'util';
+import { StreamDecrypt } from './stream_decrypt';
+
+const pipelinePromise = promisify(pipeline);
+
 describe('the StreamDecrypt class', () => {
-	it('successfully decrypts a file with a healthy input');
-	it('fails while descrypting with wrong secrets');
+	const healthyCipherIV = '44PEY4EvVXq6TuBp';
+	const badCipherIV = 'xxxxxxxxxxxxxxxx';
+	const healthyFileKey = Buffer.from('Zzaf6YeMb0chjYXjGkluCnLdufiu7/SxbuEbyPzR+1g=', 'base64');
+	const data = ':)\n\n';
+	const encryptedData = Buffer.from('boFKovaNPFrbNHt0mftHjmQexP4=', 'base64');
+	let mockStream: Readable;
+
+	beforeEach(() => {
+		mockStream = new Readable();
+		mockStream.push(encryptedData);
+		mockStream.push(null);
+	});
+
+	it('successfully decrypts a file with a healthy input', () => {
+		const decryptingStream = new StreamDecrypt(healthyCipherIV, healthyFileKey);
+		let decryptedData = '';
+		decryptingStream.on('data', (chunk: Buffer | string) => {
+			console.debug(`Data came: ${chunk}`);
+			decryptedData = `${decryptedData}${chunk.toString()}`;
+		});
+		return pipelinePromise(mockStream, decryptingStream).then((decrypted) => {
+			return expect(decryptedData).to.equal(data);
+		});
+	});
+
+	it('fails while descrypting with wrong secrets', () => {
+		const decryptingStream = new StreamDecrypt(badCipherIV, healthyFileKey);
+		return pipelinePromise(mockStream, decryptingStream).catch((err) => {
+			return expect(err).to.be.instanceOf(Error);
+		});
+	});
 });
