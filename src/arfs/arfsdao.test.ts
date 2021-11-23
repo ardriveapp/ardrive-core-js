1c2729a5216f93eacd03019df8524ac638641e8c
Derek Sonnenberg <derek@ardrive.io>
2021-11-23 15:38:31 -0600

    test(prepareObjectTransaction): Add more cases PE-726

--- src/arfs/arfsdao.test.ts
+++ src/arfs/arfsdao.test.ts
@@ -1,11 +1,12 @@
 import Arweave from 'arweave';
 import { stubEntityID } from '../../tests/stubs';
-import { ByteCount, stubTransactionID, UnixTime, W } from '../types';
+import { ByteCount, FeeMultiple, stubTransactionID, UnixTime, W } from '../types';
 import { readJWKFile } from '../utils/common';
 import { ArFSDAO } from './arfsdao';
 import { ArFSPublicFileMetaDataPrototype } from './arfs_prototypes';
 import { ArFSPublicFileMetadataTransactionData } from './arfs_trx_data_types';
 import { expect } from 'chai';
+import { Tag } from 'arweave/node/lib/transaction';
 
 describe('The ArFSDAO class', () => {
 	const wallet = readJWKFile('./test_wallet.json');
@@ -32,12 +33,34 @@ describe('The ArFSDAO class', () => {
 	);
 
 	describe('prepareObjectTransaction function', () => {
-		it('includes ArFS tag by default', async () => {
+		// Helper function to grab the decoded gql tags off of a Transaction
+		const getDecodedTagName = (tag: Tag) => tag.get('name', { decode: true, string: true });
+
+		it('includes the base ArFS tags by default', async () => {
 			const transaction = await arfsDao.prepareArFSObjectTransaction({
 				objectMetaData: stubFileMetaDataTrx,
 				rewardSettings: { reward: W(10) }
 			});
-			expect(transaction.tags.find((tag) => tag.get('name', { decode: true, string: true }) === 'ArFS')).to.exist;
+			expect(transaction.tags.find((tag) => getDecodedTagName(tag) === 'ArFS')).to.exist;
+			expect(transaction.tags.find((tag) => getDecodedTagName(tag) === 'App-Name')).to.exist;
+			expect(transaction.tags.find((tag) => getDecodedTagName(tag) === 'App-Version')).to.exist;
+		});
+
+		it('includes the boost tag when boosted', async () => {
+			const transaction = await arfsDao.prepareArFSObjectTransaction({
+				objectMetaData: stubFileMetaDataTrx,
+				rewardSettings: { reward: W(10), feeMultiple: new FeeMultiple(1.5) }
+			});
+			expect(transaction.tags.find((tag) => getDecodedTagName(tag) === 'Boost')).to.exist;
+		});
+
+		it('excludes the boost tag when boosted and boost tag is excluded', async () => {
+			const transaction = await arfsDao.prepareArFSObjectTransaction({
+				objectMetaData: stubFileMetaDataTrx,
+				rewardSettings: { reward: W(10), feeMultiple: new FeeMultiple(1.5) },
+				excludedTagNames: ['Boost']
+			});
+			expect(transaction.tags.find((tag) => getDecodedTagName(tag) === 'Boost')).to.be.undefined;
 		});
 
 		it('excludes ArFS tag if its within the exclusion array', async () => {
@@ -46,8 +69,18 @@ describe('The ArFSDAO class', () => {
 				rewardSettings: { reward: W(10) },
 				excludedTagNames: ['ArFS']
 			});
-			expect(transaction.tags.find((tag) => tag.get('name', { decode: true, string: true }) === 'ArFS')).to.be
-				.undefined;
+			expect(transaction.tags.find((tag) => getDecodedTagName(tag) === 'ArFS')).to.be.undefined;
+		});
+
+		it('can exclude multiple tags if provided within the exclusion array', async () => {
+			const transaction = await arfsDao.prepareArFSObjectTransaction({
+				objectMetaData: stubFileMetaDataTrx,
+				rewardSettings: { reward: W(10) },
+				excludedTagNames: ['ArFS', 'App-Version', 'App-Name']
+			});
+			expect(transaction.tags.find((tag) => getDecodedTagName(tag) === 'ArFS')).to.be.undefined;
+			expect(transaction.tags.find((tag) => getDecodedTagName(tag) === 'App-Name')).to.be.undefined;
+			expect(transaction.tags.find((tag) => getDecodedTagName(tag) === 'App-Version')).to.be.undefined;
 		});
 	});
 });
