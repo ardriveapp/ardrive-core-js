8a56976d205992b317c6388a6b22b5bac4d0e956
Mati <matibat@gmail.com>
2021-12-08 14:42:41 -0300

    chore(download file): removes unused source files PE-464

--- src/utils/resolve_path.test.ts
+++
@@ -1,6 +0,0 @@
-describe('resolveLocalFilePath function', () => {
-	it('returns the provided path if the file exists');
-	it('returns the provided path if the file does not exist, but the parent directory does');
-	it('returns the provided path concatenated to the default name if the path is a directory');
-	it('throws if the parent directory does not exist');
-});
