257a654bacd20ca9c976e0e99ae4c0848058a720
Derek Sonnenberg <derek@ardrive.io>
2021-12-09 21:27:55 -0600

    refactor(estimation prototypes): Add file for ArFS prototypes build for estimation PE-458

---
+++ src/utils/estimation_prototypes.ts
@@ -0,0 +1,47 @@
+import {
+	ArFSPrivateDriveMetaDataPrototype,
+	ArFSPrivateDriveTransactionData,
+	ArFSPrivateFolderMetaDataPrototype,
+	ArFSPrivateFolderTransactionData,
+	ArFSPublicDriveMetaDataPrototype,
+	ArFSPublicDriveTransactionData,
+	ArFSPublicFolderMetaDataPrototype,
+	ArFSPublicFolderTransactionData,
+	CreatePrivateDriveParams,
+	CreatePublicDriveParams
+} from '../exports';
+import { EstimateCreateDriveParams } from '../types/cost_estimator_types';
+import { fakeEntityId } from './constants';
+
+export async function getPrivateCreateDriveEstimationPrototypes({
+	driveName,
+	newPrivateDriveData
+}: CreatePrivateDriveParams): Promise<EstimateCreateDriveParams> {
+	return {
+		rootFolderMetaDataPrototype: new ArFSPrivateFolderMetaDataPrototype(
+			fakeEntityId,
+			fakeEntityId,
+			await ArFSPrivateFolderTransactionData.from(driveName, newPrivateDriveData.driveKey)
+		),
+		driveMetaDataPrototype: new ArFSPrivateDriveMetaDataPrototype(
+			fakeEntityId,
+			await ArFSPrivateDriveTransactionData.from(driveName, fakeEntityId, newPrivateDriveData.driveKey)
+		)
+	};
+}
+
+export function getPublicCreateDriveEstimationPrototypes({
+	driveName
+}: CreatePublicDriveParams): EstimateCreateDriveParams {
+	return {
+		rootFolderMetaDataPrototype: new ArFSPublicFolderMetaDataPrototype(
+			new ArFSPublicFolderTransactionData(driveName),
+			fakeEntityId,
+			fakeEntityId
+		),
+		driveMetaDataPrototype: new ArFSPublicDriveMetaDataPrototype(
+			new ArFSPublicDriveTransactionData(driveName, fakeEntityId),
+			fakeEntityId
+		)
+	};
+}
