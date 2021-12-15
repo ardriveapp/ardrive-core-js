456c5c44a2650a56638c0accb4293b6dc3da6dbc
Derek Sonnenberg <derek@ardrive.io>
2021-12-15 16:07:14 -0600

    feat(tag builder): Assert tag limitations on tag assembly PE-458

--- src/arfs/arfs_tag_settings.ts
+++ src/arfs/arfs_tag_settings.ts
@@ -1,6 +1,13 @@
 import { GQLTagInterface, TipType } from '../types';
 import { CURRENT_ARFS_VERSION, DEFAULT_APP_NAME, DEFAULT_APP_VERSION } from '../utils/constants';
 
+// Tag Limits to be in compliance with ANS-104:
+// https://github.com/joshbenaron/arweave-standards/blob/ans104/ans/ANS-104.md#21-verifying-a-dataitem
+const MAX_TAG_LIMIT = 128;
+const TAG_NAME_BYTE_LIMIT = 1024;
+const TAG_VALUE_BYTE_LIMIT = 3072;
+const TAG_KEY_LIMIT = 2;
+
 interface ArFSTagSettingsParams {
 	appName?: string;
 	appVersion?: string;
@@ -20,6 +27,8 @@ export class ArFSTagSettings {
 		this.appName = appName;
 		this.appVersion = appVersion;
 		this.arFSVersion = arFSVersion;
+
+		this.assertTagLimits(this.baseArFSTags);
 	}
 
 	get baseAppTags(): GQLTagInterface[] {
@@ -47,12 +56,55 @@ export class ArFSTagSettings {
 
 	assembleBaseArFSTags({ tags = [], excludedTagNames = [] }: TagAssembleParams): GQLTagInterface[] {
 		tags = [...this.baseArFSTags, ...tags];
-		return this.filterExcludedTagNames({ tags, excludedTagNames });
+
+		return this.assembleTags({ tags, excludedTagNames });
 	}
 
 	assembleBaseBundleTags({ tags = [], excludedTagNames = [] }: TagAssembleParams): GQLTagInterface[] {
 		tags = [...this.baseBundleTags, ...tags];
-		return this.filterExcludedTagNames({ tags, excludedTagNames });
+
+		return this.assembleTags({ tags, excludedTagNames });
+	}
+
+	private assembleTags({ tags = [], excludedTagNames = [] }: TagAssembleParams): GQLTagInterface[] {
+		tags = this.filterExcludedTagNames({ tags, excludedTagNames });
+
+		this.assertTagLimits(tags);
+
+		return tags;
+	}
+
+	assertTagLimits(tags: GQLTagInterface[]): void {
+		if (tags.length > MAX_TAG_LIMIT) {
+			throw new Error(
+				`Amount of GQL Tags (${tags.length}) exceeds the maximum limit allowed (${MAX_TAG_LIMIT})!`
+			);
+		}
+		for (const tag of tags) {
+			if (Object.keys(tag).length > TAG_KEY_LIMIT) {
+				throw new Error('GQL tag has too many keys, tags must only have "name" and "value" fields!');
+			}
+
+			if (tag.name.length > TAG_NAME_BYTE_LIMIT) {
+				throw new Error(
+					`GQL tag "name" field byte size (${tag.name.length}) has exceeded the maximum byte limit allowed of ${TAG_NAME_BYTE_LIMIT}!`
+				);
+			}
+
+			if (tag.value.length > TAG_VALUE_BYTE_LIMIT) {
+				throw new Error(
+					`GQL tag "value" field byte size (${tag.value.length}) has exceeded the maximum byte limit allowed of ${TAG_VALUE_BYTE_LIMIT}!`
+				);
+			}
+
+			if (tag.name.length < 1 || typeof tag.name !== 'string') {
+				throw new Error('GQL tag "name" must be a non-empty string!');
+			}
+
+			if (tag.value.length < 1 || typeof tag.value !== 'string') {
+				throw new Error('GQL tag "value" must be a non-empty string!');
+			}
+		}
 	}
 
 	private filterExcludedTagNames({ tags = [], excludedTagNames = [] }: TagAssembleParams): GQLTagInterface[] {
