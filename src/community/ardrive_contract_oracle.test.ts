484155898151e7891b1332cf0492573d59e4e518
Derek Sonnenberg <derek@ardrive.io>
2021-12-20 22:27:50 -0600

    test(community oracle): Add coverage for community oracle PE-459

--- src/community/ardrive_contract_oracle.test.ts
+++ src/community/ardrive_contract_oracle.test.ts
@@ -1,64 +1,17 @@
 import { expect } from 'chai';
 import { spy } from 'sinon';
-import { stubArweaveAddress, stubTxID } from '../../tests/stubs';
+import { stubCommunityContract, stubTxID } from '../../tests/stubs';
 import { expectAsyncErrorThrow } from '../../tests/test_helpers';
 import { ArDriveContractOracle } from './ardrive_contract_oracle';
 
 describe('The ArDriveContractOracle', () => {
-	const stubCommunityContract = {
-		settings: [['fee', 15]],
-		vault: { [`${stubArweaveAddress}`]: [{ balance: 500, start: 1, end: 2 }] },
-		balances: { [`${stubArweaveAddress}`]: 200 }
-	};
-	const stubContractWithNoFee = {
-		...stubCommunityContract,
-		settings: [['not-a-fee', 'lol']]
-	};
-	const stubContractWithFeeAsString = {
-		...stubCommunityContract,
-		settings: [['fee', 'STUB_STRING']]
-	};
-	const stubContractWithNegativeFee = {
-		...stubCommunityContract,
-		settings: [['fee', -600]]
-	};
-
 	const stubContractReader = {
 		async readContract() {
 			return stubCommunityContract;
 		}
 	};
-	const stubContractReaderWithNoFee = {
-		async readContract() {
-			return stubContractWithNoFee;
-		}
-	};
-	const stubContractReaderWithFeeAsString = {
-		async readContract() {
-			return stubContractWithFeeAsString;
-		}
-	};
-	const stubContractReaderWithNegativeFee = {
-		async readContract() {
-			return stubContractWithNegativeFee;
-		}
-	};
-
-	const errorThrowingStubContractReader = {
-		async readContract() {
-			throw new Error('Big time fail!');
-		}
-	};
-	const arDriveContractOracleWithError = new ArDriveContractOracle([errorThrowingStubContractReader]);
-	const arDriveContractOracleWithFallback = new ArDriveContractOracle([
-		errorThrowingStubContractReader,
-		stubContractReader
-	]);
 
 	const arDriveContractOracle = new ArDriveContractOracle([stubContractReader]);
-	const arDriveContractOracleWithNoFee = new ArDriveContractOracle([stubContractReaderWithNoFee]);
-	const arDriveContractOracleWithFeeAsString = new ArDriveContractOracle([stubContractReaderWithFeeAsString]);
-	const arDriveContractOracleWithNegativeFee = new ArDriveContractOracle([stubContractReaderWithNegativeFee]);
 
 	describe('constructor', () => {
 		it('does not read the community contract on construction by default', () => {
@@ -76,10 +29,20 @@ describe('The ArDriveContractOracle', () => {
 
 	describe('getPercentageFromContract method', () => {
 		it('returns the expected fee result', async () => {
-			expect(await arDriveContractOracle.getTipPercentageFromContract()).to.equal(0.15);
+			expect(await arDriveContractOracle.getTipPercentageFromContract()).to.equal(0.5);
 		});
 
 		it('throws an error if fee does not exist', async () => {
+			const stubContractReaderWithNoFee = {
+				async readContract() {
+					return {
+						...stubCommunityContract,
+						settings: [['not-a-fee', 'lol']]
+					};
+				}
+			};
+			const arDriveContractOracleWithNoFee = new ArDriveContractOracle([stubContractReaderWithNoFee]);
+
 			await expectAsyncErrorThrow({
 				promiseToError: arDriveContractOracleWithNoFee.getTipPercentageFromContract(),
 				errorMessage: 'Fee does not exist on smart contract settings'
@@ -87,6 +50,16 @@ describe('The ArDriveContractOracle', () => {
 		});
 
 		it('throws an error if fee is not a number', async () => {
+			const stubContractReaderWithFeeAsString = {
+				async readContract() {
+					return {
+						...stubCommunityContract,
+						settings: [['fee', 'STUB_STRING']]
+					};
+				}
+			};
+			const arDriveContractOracleWithFeeAsString = new ArDriveContractOracle([stubContractReaderWithFeeAsString]);
+
 			await expectAsyncErrorThrow({
 				promiseToError: arDriveContractOracleWithFeeAsString.getTipPercentageFromContract(),
 				errorMessage: 'Fee on smart contract settings is not a number'
@@ -94,6 +67,16 @@ describe('The ArDriveContractOracle', () => {
 		});
 
 		it('throws an error if fee is not a number', async () => {
+			const stubContractReaderWithNegativeFee = {
+				async readContract() {
+					return {
+						...stubCommunityContract,
+						settings: [['fee', -600]]
+					};
+				}
+			};
+			const arDriveContractOracleWithNegativeFee = new ArDriveContractOracle([stubContractReaderWithNegativeFee]);
+
 			await expectAsyncErrorThrow({
 				promiseToError: arDriveContractOracleWithNegativeFee.getTipPercentageFromContract(),
 				errorMessage: 'Fee on smart contract community settings is set to a negative number'
@@ -106,7 +89,15 @@ describe('The ArDriveContractOracle', () => {
 			expect(await arDriveContractOracle.readContract(stubTxID)).to.deep.equal(stubCommunityContract);
 		});
 
+		const errorThrowingStubContractReader = {
+			async readContract() {
+				throw new Error('Big time fail!');
+			}
+		};
+
 		it('throws an error if contract cannot be resolved by any contract reader', async () => {
+			const arDriveContractOracleWithError = new ArDriveContractOracle([errorThrowingStubContractReader]);
+
 			await expectAsyncErrorThrow({
 				promiseToError: arDriveContractOracleWithError.readContract(stubTxID),
 				errorMessage: 'Max contract read attempts has been reached on the last fallback contract reader..'
@@ -114,6 +105,11 @@ describe('The ArDriveContractOracle', () => {
 		});
 
 		it('falls back to the next contract reader on error and returns the expected stub community contract', async () => {
+			const arDriveContractOracleWithFallback = new ArDriveContractOracle([
+				errorThrowingStubContractReader,
+				stubContractReader
+			]);
+
 			expect(await arDriveContractOracleWithFallback.readContract(stubTxID)).to.deep.equal(stubCommunityContract);
 		});
 	});
