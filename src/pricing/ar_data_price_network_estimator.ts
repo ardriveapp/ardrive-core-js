1eeb43609820fd7837fbce9a77844e4919501f97
Derek Sonnenberg <derek@ardrive.io>
2021-12-22 10:08:28 -0600

    refactor(network price estimator): Move caching layer to new CachedGatewayOracle PE-829

--- src/pricing/ar_data_price_network_estimator.ts
+++ src/pricing/ar_data_price_network_estimator.ts
@@ -1,52 +1,30 @@
-import { GatewayOracle } from './gateway_oracle';
+import { CachedGatewayOracle } from './gateway_oracle';
 import type { ArweaveOracle } from './arweave_oracle';
 import { AbstractARDataPriceAndCapacityEstimator } from './ar_data_price_estimator';
-import { AR, ByteCount, Winston, ArDriveCommunityTip } from '../types';
-
-const byteCountPerChunk = new ByteCount(Math.pow(2, 10) * 256); // 256 KiB
+import { ByteCount, W, Winston } from '../types';
+import { byteCountPerChunk } from '../utils/constants';
 
 /**
  * A utility class for Arweave data pricing estimation.
  * Fetches Arweave pricing and caches results based on their chunk size
  */
 export class ARDataPriceNetworkEstimator extends AbstractARDataPriceAndCapacityEstimator {
-	private bytesToChunks(bytes: ByteCount): number {
-		return Math.ceil(+bytes / +byteCountPerChunk);
-	}
-
-	private cachedPricePerChunk: { [chunks: string]: Promise<Winston> } = {};
-
 	/**
 	 * Creates a new estimator.
 	 *
 	 * @param oracle a data source for Arweave data pricing
 	 */
-	constructor(private readonly oracle: ArweaveOracle = new GatewayOracle()) {
+	constructor(private readonly oracle: ArweaveOracle = new CachedGatewayOracle()) {
 		super();
 	}
 
 	/**
-	 * Generates a price estimate, in Winston, for an upload of size `byteCount`.
+	 * Goes to the arweave gateway oracle a price estimate, in Winston, for an upload of size `byteCount`.
 	 *
 	 * @param byteCount the number of bytes for which a price estimate should be generated
-	 *
-	 * @returns Promise for the price of an upload of size `byteCount` in Winston
-	 *
-	 * @remarks Will use cached price first for a given chunk size
-	 *
 	 */
 	public async getBaseWinstonPriceForByteCount(byteCount: ByteCount): Promise<Winston> {
-		const chunks = this.bytesToChunks(byteCount);
-		const cachedPrice = this.cachedPricePerChunk[`${chunks}`];
-
-		if (cachedPrice) {
-			return cachedPrice;
-		}
-
-		const winstonPricePromise = this.oracle.getWinstonPriceForByteCount(byteCount);
-		this.cachedPricePerChunk[`${chunks}`] = winstonPricePromise;
-
-		return winstonPricePromise;
+		return this.oracle.getWinstonPriceForByteCount(byteCount);
 	}
 
 	/**
@@ -60,7 +38,7 @@ export class ARDataPriceNetworkEstimator extends AbstractARDataPriceAndCapacityE
 		const oneChunkWinstonPrice = await this.getBaseWinstonPriceForByteCount(new ByteCount(1));
 
 		if (winston.isGreaterThanOrEqualTo(oneChunkWinstonPrice)) {
-			const perChunkEstCost = oneChunkWinstonPrice.minus(baseWinstonPrice);
+			const perChunkEstCost = oneChunkWinstonPrice.minus(baseWinstonPrice).plus(W(1));
 			const estimatedChunks = Math.floor(
 				+winston.minus(baseWinstonPrice).dividedBy(+perChunkEstCost, 'ROUND_DOWN')
 			);
@@ -70,17 +48,5 @@ export class ARDataPriceNetworkEstimator extends AbstractARDataPriceAndCapacityE
 
 		// Return 0 if winston price given does not cover the base winston price for a 1 chunk transaction
 		return new ByteCount(0);
-	}
-
-	/**
-	 * Estimates the number of bytes that can be stored for a given amount of AR
-	 *
-	 * @remarks Returns 0 bytes when the price does not cover minimum ArDrive community fee
-	 */
-	public async getByteCountForAR(
-		arPrice: AR,
-		{ minWinstonFee, tipPercentage }: ArDriveCommunityTip
-	): Promise<ByteCount> {
-		return super.getByteCountForAR(arPrice, { minWinstonFee, tipPercentage });
 	}
 }
--- src/pricing/gateway_oracle.ts
+++ src/pricing/gateway_oracle.ts
@@ -2,11 +2,42 @@ import type { ArweaveOracle } from './arweave_oracle';
 import { ByteCount, W, Winston } from '../types';
 import { BigNumber } from 'bignumber.js';
 import axios, { AxiosResponse } from 'axios';
+import { bytesToChunks } from '../utils/common';
 
 export class GatewayOracle implements ArweaveOracle {
+	constructor(private readonly axiosRequest: (url: string) => Promise<AxiosResponse> = (url) => axios.get(url)) {}
+
 	async getWinstonPriceForByteCount(byteCount: ByteCount): Promise<Winston> {
-		const response: AxiosResponse = await axios.get(`https://arweave.net/price/${byteCount}`);
+		const response: AxiosResponse = await this.axiosRequest(`https://arweave.net/price/${byteCount}`);
 		const winstonAsString = `${response.data}`;
 		return W(new BigNumber(winstonAsString));
+	}
+}
+
+export class CachedGatewayOracle extends GatewayOracle {
+	private initialPriceRequest = true;
+
+	private cachedPricePerChunk: { [chunks: string]: Promise<Winston> } = {};
+
+	async getWinstonPriceForByteCount(byteCount: ByteCount): Promise<Winston> {
+		if (this.initialPriceRequest) {
+			// Cache promises for 0 and 1 chunk for chunk estimator calculations on initial request
+			this.cachedPricePerChunk['0'] = super.getWinstonPriceForByteCount(new ByteCount(0));
+			this.cachedPricePerChunk['1'] = super.getWinstonPriceForByteCount(new ByteCount(1));
+			this.initialPriceRequest = false;
+		}
+
+		const chunks = bytesToChunks(byteCount);
+		const cachedPrice = this.cachedPricePerChunk[`${chunks}`];
+
+		if (cachedPrice) {
+			return cachedPrice;
+		}
+
+		const winstonPricePromise = super.getWinstonPriceForByteCount(byteCount);
+
+		this.cachedPricePerChunk[`${chunks}`] = winstonPricePromise;
+
+		return winstonPricePromise;
 	}
 }
