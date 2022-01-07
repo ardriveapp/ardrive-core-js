127e32e1ac56faa454af3064f0d56a981fe2e51d
Derek Sonnenberg <derek@ardrive.io>
2022-01-07 09:00:59 -0600

    feat(bulk file bundle): Initialize bundle packer PE-460

---
+++ src/utils/bundle_packer.ts
@@ -0,0 +1,70 @@
+import { MAX_BUNDLE_SIZE, MAX_DATA_ITEM_LIMIT } from '../arfs/arfs_upload_planner';
+import { ByteCount, UploadOrder } from '../types';
+
+export type BundleIndex = number;
+
+interface BundlePackParams {
+	// Upload order can be undefined when a metaData for an over-sized file is packed
+	uploadOrder?: UploadOrder;
+	byteCountAsDataItem: ByteCount;
+	numberOfDataItems: number;
+}
+
+export abstract class BundlePacker {
+	public bundles: BundleToPack[] = [];
+
+	abstract packIntoBundle(bundlePackParams: BundlePackParams): BundleIndex;
+}
+
+/**
+ * Pack into lowest index bundle with available size and remaining data items
+ *
+ * Preserve the BundleIndex for use in edge case where FileData is above MAX_BUNDLE_SIZE
+ * but the fileMetaData will still be sent up with a bundle
+ */
+export class LowestIndexBundlePacker extends BundlePacker {
+	packIntoBundle(bundlePackParams: BundlePackParams): BundleIndex {
+		const { byteCountAsDataItem, numberOfDataItems } = bundlePackParams;
+
+		for (let index = 0; index < this.bundles.length; index++) {
+			const bundle = this.bundles[index];
+			// Pack into lowest index bundle that has enough remaining size and data items
+			if (+byteCountAsDataItem <= bundle.remainingSize && numberOfDataItems <= bundle.remainingDataItems) {
+				bundle.addToBundle(bundlePackParams);
+				return index;
+			}
+		}
+
+		// Otherwise we pack into a new bundle
+		this.bundles.push(new BundleToPack(bundlePackParams));
+		return this.bundles.length - 1;
+	}
+}
+
+class BundleToPack {
+	public uploadOrders: UploadOrder[] = [];
+
+	public totalSize = 0;
+	public totalDataItems = 0;
+
+	get remainingSize() {
+		return MAX_BUNDLE_SIZE - this.totalSize;
+	}
+	get remainingDataItems() {
+		return MAX_DATA_ITEM_LIMIT - this.totalDataItems;
+	}
+
+	constructor(initialBundlePackParams: BundlePackParams) {
+		this.addToBundle(initialBundlePackParams);
+	}
+
+	addToBundle({ uploadOrder, byteCountAsDataItem, numberOfDataItems }: BundlePackParams) {
+		this.totalSize += +byteCountAsDataItem;
+		this.totalDataItems += numberOfDataItems;
+
+		// Metadata of over-sized file uploads can be added without an uploadOrder
+		if (uploadOrder) {
+			this.uploadOrders.push(uploadOrder);
+		}
+	}
+}
