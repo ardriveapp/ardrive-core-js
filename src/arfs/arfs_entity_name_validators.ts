34a08f9d4a3e0f2cebb77d2ac72a1eb7ef8de2c7
Mati <matibat@gmail.com>
2022-01-19 15:11:17 -0300

    feat(rename file): more robust and simpler error handling in the name validator PE-878

--- src/arfs/arfs_entity_name_validators.ts
+++ src/arfs/arfs_entity_name_validators.ts
@@ -1,21 +1,31 @@
 const RESERVED_CHARACTERS = ['\\\\', '/', ':', '*', '?', '"', '<', '>', '|'];
-/* RegExp explanation
- * - `^$` matches the beginning and the end of the string
- * - for each character, we ensure it is not a reserved one, and
- *   - that it's not preceded by a space at the beginning
- *   - nor a space or point at the end
- */
-const VALID_FILE_NAME_REGEXP = new RegExp(`^(?:(?!^\\s)[^${RESERVED_CHARACTERS.join('')}](?![. ]$))+$`);
+const HUMAN_READABLE_RESERVED_CHARACTERS = RESERVED_CHARACTERS.map((char) => `'${char}'`).join(', ');
+
+const LEADING_SPACES_REGEXP = /^(\s+)/;
+const TRAILING_DOTS_OR_SPACES_REGEXP = /(\s|\.)+$/;
+const CONTAINS_RESERVED_CHARACTER_REGEXP = new RegExp(`[${RESERVED_CHARACTERS.join('')}]`, 'g');
+// From ArFS Standards
 const MAX_VALID_NAME_LENGTH = 255;
 
 export function assertValidArFSFileName(name: string): boolean {
 	if (name.length > MAX_VALID_NAME_LENGTH || name.length === 0) {
 		throw new Error(`The file name must contain between 0 and ${MAX_VALID_NAME_LENGTH} characters`);
 	}
-	const match = name.match(VALID_FILE_NAME_REGEXP);
-	if (!match) {
+
+	const matchLeadingSpaces = name.match(LEADING_SPACES_REGEXP);
+	if (matchLeadingSpaces) {
+		throw new Error(`The file name cannot start with spaces`);
+	}
+
+	const matchTrailingDotsOrSpaces = name.match(TRAILING_DOTS_OR_SPACES_REGEXP);
+	if (matchTrailingDotsOrSpaces) {
+		throw new Error(`The file name cannot have trailing dots or spaces`);
+	}
+
+	const matchReservedCharacters = name.match(CONTAINS_RESERVED_CHARACTER_REGEXP);
+	if (matchReservedCharacters) {
 		throw new Error(
-			`The file name cannot contain reserved characters, cannot start with space or end with a dot or space`
+			`The file name cannot contain reserved characters (i.e. ${HUMAN_READABLE_RESERVED_CHARACTERS})`
 		);
 	}
 	return true;
