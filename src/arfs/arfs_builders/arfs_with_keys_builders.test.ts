da7a7002cfad7b0b8825ad88917852e8c06cebf7
Mati <matibat@gmail.com>
2022-02-03 18:06:10 -0300

    test(entities with keys): adds some pending unit tests PE-970

---
+++ src/arfs/arfs_builders/arfs_with_keys_builders.test.ts
@@ -0,0 +1,4 @@
+describe('ArFSPrivateFileOrFolderWithPathsAndKeysBuider class', () => {
+	it('Built folders will have the extra driveKey attribute set');
+	it('Built files will have the driveKey and fileKey attributes set');
+});
