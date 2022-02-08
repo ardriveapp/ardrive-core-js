91f968e07d3a18a1f5215b72a023379998fa9d5c
Mati <matibat@gmail.com>
2022-02-08 14:44:19 -0300

    chore(entities with keys): removes the unused builder for the WithKeys entities PE-971

--- src/arfs/arfs_builders/arfs_with_keys_builders.test.ts
+++
@@ -1,4 +0,0 @@
-describe('ArFSPrivateFileOrFolderWithPathsAndKeysBuider class', () => {
-	it('Built folders will have the extra driveKey attribute set');
-	it('Built files will have the driveKey and fileKey attributes set');
-});
