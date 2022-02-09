a7172e31dab280410658248c274100bb031d6413
Karl Prieb <karl@prieb.me>
2022-02-09 14:26:56 -0300

    fix(entity name validation): file name byte length

--- src/arfs/arfs_entity_name_validators.ts
+++ src/arfs/arfs_entity_name_validators.ts
@@ -2,17 +2,23 @@ import { EntityType } from '../types';
 import { ArFSFolderToUpload } from '../arfs/arfs_file_wrapper';
 
 // From ArFS Standards
-const MAX_VALID_NAME_LENGTH = 255;
+const MAX_VALID_NAME_BYTE_LENGTH = 255;
 
 export const assertValidArFSFileName = assertValidArFSEntityNameFactory('file');
 export const assertValidArFSFolderName = assertValidArFSEntityNameFactory('folder');
 export const assertValidArFSDriveName = assertValidArFSEntityNameFactory('drive');
 
 export function assertValidArFSEntityNameFactory(entityType: EntityType): (name: string) => void | Error {
-	return function (name: string) {
-		// Check for max length and empty names
-		if (name.length > MAX_VALID_NAME_LENGTH || name.length === 0) {
-			throw new Error(`The ${entityType} name must contain between 1 and ${MAX_VALID_NAME_LENGTH} characters`);
+	return function (name: string): void {
+		// Check for empy names
+		if (name.length === 0) {
+			throw new Error(`The ${entityType} name cannot be empty`);
+		}
+
+		// Check for max byte length
+		const nameByteLength = new TextEncoder().encode(name).length;
+		if (nameByteLength > MAX_VALID_NAME_BYTE_LENGTH) {
+			throw new Error(`The ${entityType} name must be smaller than ${MAX_VALID_NAME_BYTE_LENGTH} bytes`);
 		}
 
 		// Check for null characters
