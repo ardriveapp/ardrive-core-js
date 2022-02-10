a340cbe1ac70309e7ce9db4bf52a59c3688c004e
Mati <matibat@gmail.com>
2022-02-10 13:17:53 -0300

    chore(folder hierarchy): clarifies variable name PE-981

--- src/arfs/folder_hierarchy.test.ts
+++ src/arfs/folder_hierarchy.test.ts
@@ -68,24 +68,24 @@ describe('FolderHierarchy class', () => {
 	];
 	const stubFolderIDsWithoutRoot = stubFolderIDs.slice(1);
 
-	let stubHierarchy: FolderHierarchy;
-	let stubHierarchyWithoutRoot: FolderHierarchy;
+	let folderHierarchy: FolderHierarchy;
+	let folderHierarchyWithoutRoot: FolderHierarchy;
 
 	before(() => {
-		stubHierarchy = FolderHierarchy.newFromEntities(allFolders);
-		stubHierarchyWithoutRoot = stubHierarchy.subTreeOf(stubEntityIDParent, Number.MAX_SAFE_INTEGER);
+		folderHierarchy = FolderHierarchy.newFromEntities(allFolders);
+		folderHierarchyWithoutRoot = folderHierarchy.subTreeOf(stubEntityIDParent, Number.MAX_SAFE_INTEGER);
 	});
 
 	it('rootNode attribute always returns a FolderTreeNode', () => {
-		const treeRoot = stubHierarchy.rootNode;
-		const subTreeRoot = stubHierarchyWithoutRoot.rootNode;
+		const treeRoot = folderHierarchy.rootNode;
+		const subTreeRoot = folderHierarchyWithoutRoot.rootNode;
 		expect(treeRoot).to.be.instanceOf(FolderTreeNode);
 		expect(subTreeRoot).to.be.instanceOf(FolderTreeNode);
 	});
 
 	it('subTreeOf returns a new FolderHierarchy instance with the correct root node', () => {
-		const subTree = stubHierarchy.subTreeOf(stubEntityIDParent);
-		const subTreeAlt = stubHierarchyWithoutRoot.subTreeOf(stubEntityIDParent);
+		const subTree = folderHierarchy.subTreeOf(stubEntityIDParent);
+		const subTreeAlt = folderHierarchyWithoutRoot.subTreeOf(stubEntityIDParent);
 		expect(subTree).to.be.instanceOf(FolderHierarchy);
 		expect(subTreeAlt).to.be.instanceOf(FolderHierarchy);
 		expect(subTree.rootNode).to.be.instanceOf(FolderTreeNode);
@@ -93,25 +93,25 @@ describe('FolderHierarchy class', () => {
 	});
 
 	it('allFolderIDs returns an array of FolderIDs', () => {
-		const allFolderIDs = stubHierarchy.allFolderIDs();
+		const allFolderIDs = folderHierarchy.allFolderIDs();
 		// Ensures the array deeply (but not strictly) has members
 		expect(allFolderIDs).to.have.deep.members(stubFolderIDs);
-		const allFolderIDsWithoutRoot = stubHierarchyWithoutRoot.allFolderIDs();
+		const allFolderIDsWithoutRoot = folderHierarchyWithoutRoot.allFolderIDs();
 		// Ensures the array deeply (but not strictly) has members
 		expect(allFolderIDsWithoutRoot).to.have.deep.members(stubFolderIDsWithoutRoot);
 	});
 
 	it('nodeAndChildrenOf returns a flattened array of FolderTreeNode including the given node', () => {
-		const rootNode = stubHierarchy.rootNode;
-		const nodes = stubHierarchy.nodeAndChildrenOf(rootNode, Number.MAX_SAFE_INTEGER);
+		const rootNode = folderHierarchy.rootNode;
+		const nodes = folderHierarchy.nodeAndChildrenOf(rootNode, Number.MAX_SAFE_INTEGER);
 		expect(nodes).to.have.lengthOf(6);
 		expect(nodes).to.include(rootNode);
 		nodes.forEach((node) => expect(node).to.be.instanceOf(FolderTreeNode));
 	});
 
 	it('folderIdSubtreeFromFolderId return a flattened array of FolderIDs starting from the given node', () => {
-		const rootNode = stubHierarchy.rootNode;
-		const folderIDs = stubHierarchy.folderIdSubtreeFromFolderId(rootNode.folderId, Number.MAX_SAFE_INTEGER);
+		const rootNode = folderHierarchy.rootNode;
+		const folderIDs = folderHierarchy.folderIdSubtreeFromFolderId(rootNode.folderId, Number.MAX_SAFE_INTEGER);
 		expect(folderIDs).to.have.lengthOf(6);
 		expect(folderIDs).to.include(rootNode.folderId);
 		folderIDs.forEach((entityID) => {
@@ -120,37 +120,37 @@ describe('FolderHierarchy class', () => {
 	});
 
 	it('pathToFolderId throws if the rootNode of the whole HierarchyTree is not the root of the drive', () => {
-		expect(() => stubHierarchyWithoutRoot.pathToFolderId(stubEntityIDGrandchild)).to.throw(
+		expect(() => folderHierarchyWithoutRoot.pathToFolderId(stubEntityIDGrandchild)).to.throw(
 			"Can't compute paths from sub-tree"
 		);
 	});
 
 	it('pathToFolderId returns a path-like string', () => {
-		const namesPath = stubHierarchy.pathToFolderId(stubEntityIDGrandchild);
+		const namesPath = folderHierarchy.pathToFolderId(stubEntityIDGrandchild);
 		expect(namesPath).to.equal('/The drive name/Funny stuff/Funny folder #1/The funniest folder/');
 	});
 
 	it('entityPathToFolderId throws if the rootNode of the whole HierarchyTree is not the root of the drive', () => {
-		expect(() => stubHierarchyWithoutRoot.entityPathToFolderId(stubEntityIDGrandchild)).to.throw(
+		expect(() => folderHierarchyWithoutRoot.entityPathToFolderId(stubEntityIDGrandchild)).to.throw(
 			"Can't compute paths from sub-tree"
 		);
 	});
 
 	it('entityPathToFolderId returns a path-like string', () => {
-		const namesPath = stubHierarchy.entityPathToFolderId(stubEntityIDGrandchild);
+		const namesPath = folderHierarchy.entityPathToFolderId(stubEntityIDGrandchild);
 		expect(namesPath).to.equal(
 			'/00000000-0000-0000-0000-000000000002/00000000-0000-0000-0000-000000000003/00000000-0000-0000-0000-000000000000/00000000-0000-0000-0000-000000000005/'
 		);
 	});
 
 	it('txPathToFolderId throws if the rootNode of the whole HierarchyTree is not the root of the drive', () => {
-		expect(() => stubHierarchyWithoutRoot.txPathToFolderId(stubEntityIDGrandchild)).to.throw(
+		expect(() => folderHierarchyWithoutRoot.txPathToFolderId(stubEntityIDGrandchild)).to.throw(
 			"Can't compute paths from sub-tree"
 		);
 	});
 
 	it('txPathToFolderId returns a path-like string', () => {
-		const namesPath = stubHierarchy.txPathToFolderId(stubEntityIDGrandchild);
+		const namesPath = folderHierarchy.txPathToFolderId(stubEntityIDGrandchild);
 		expect(namesPath).to.equal(
 			'/0000000000000000000000000000000000000000000/0000000000000000000000000000000000000000000/0000000000000000000000000000000000000000000/0000000000000000000000000000000000000000000/'
 		);
@@ -163,7 +163,7 @@ describe('FolderHierarchy class', () => {
 
 		it('Can be constructed from a folderId', () => {
 			expect(() => new FolderTreeNode(stubEntityID)).to.not.throw();
-			expect(() => new FolderTreeNode(stubEntityID, stubHierarchy.rootNode)).to.not.throw();
+			expect(() => new FolderTreeNode(stubEntityID, folderHierarchy.rootNode)).to.not.throw();
 		});
 	});
 });
