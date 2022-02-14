3ee9df3c7a14a97d29153e5ea8fb23b49153a9fa
Derek Sonnenberg <derek@ardrive.io>
2022-02-14 15:22:51 -0600

    test(arlocal): Adjust docker script PE-480

--- tests/arlocal-docker-test.sh
+++ tests/arlocal-docker-test.sh
@@ -2,10 +2,14 @@
 
 docker run --init --rm -d -p 1984:1984 --name arlocal textury/arlocal:latest
 
+set -e
+EXIT_CODE=0
+
 # Add grep support for arlocal mocha tests
 if [ -z "$1" ];
-then yarn nyc mocha; 
-else yarn nyc mocha -g "$1"; 
+then yarn nyc mocha || EXIT_CODE=$?; 
+else yarn nyc mocha -g "$1" || EXIT_CODE=$?; 
 fi
 
 docker stop arlocal
+echo $EXIT_CODE
