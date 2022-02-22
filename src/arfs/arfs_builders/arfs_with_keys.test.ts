46f0f9bf0dc8f682fefdfcaf154d7e8a99d56bfb
Mati <matibat@gmail.com>
2022-02-22 07:51:25 -0300

    test(entities with keys): test cleanup PE-981

--- src/arfs/arfs_builders/arfs_with_keys.test.ts
+++ src/arfs/arfs_builders/arfs_with_keys.test.ts
@@ -11,13 +11,14 @@ import {
 	stubPrivateFolder
 } from '../../../tests/stubs';
 import {
+	ArFSPrivateFile,
 	ArFSPrivateFileWithPaths,
+	ArFSPrivateFolder,
 	FolderHierarchy,
 	privateEntityWithPathsFactory,
 	privateEntityWithPathsKeylessFactory
 } from '../../exports';
 import { DriveKey, FileKey } from '../../types';
-import { urlEncodeHashKey } from '../../utils/common';
 import { RootFolderID } from './arfs_folder_builders';
 
 describe('Entities with paths classes', () => {
@@ -31,84 +32,81 @@ describe('Entities with paths classes', () => {
 	 * - The drive name/Funny stuff/Funny folder #1/jokes.pdf
 	 */
 
-	// Root folder
-	const stubRootFolder = stubPrivateFolder({
-		folderId: stubEntityIDRoot,
-		parentFolderId: new RootFolderID(),
-		folderName: 'The drive name'
-	});
-	// Depth 0
-	const stubParentFolder = stubPrivateFolder({
-		folderId: stubEntityIDParent,
-		parentFolderId: stubEntityIDRoot,
-		folderName: 'Funny stuff'
-	});
-	// Depth 1
-	const stubFolder_0 = stubPrivateFolder({
-		folderId: stubEntityID,
-		parentFolderId: stubEntityIDParent,
-		folderName: 'Funny folder #1'
-	});
-	const stubFolder_1 = stubPrivateFolder({
-		folderId: stubEntityIDAlt,
-		parentFolderId: stubEntityIDParent,
-		folderName: 'Funny folder #2'
-	});
-	// Depth 2
-	const stubFolder_3 = stubPrivateFolder({
-		folderId: stubEntityIDGrandchild,
-		parentFolderId: stubEntityID,
-		folderName: 'The funniest folder'
-	});
-	const stubFile = stubPrivateFile({
-		fileId: stubEntityIDAltTwo,
-		fileName: 'jokes.pdf'
-	});
+	// Root folder (Depth 0)
+	let stubRootFolder: ArFSPrivateFolder;
+	let stubParentFolder: ArFSPrivateFolder;
+	let stubFolder_0: ArFSPrivateFolder;
+	let stubFolder_1: ArFSPrivateFolder;
+	let stubFolder_3: ArFSPrivateFolder;
+	let stubFile: ArFSPrivateFile;
 
-	const allFolders = [stubRootFolder, stubParentFolder, stubFolder_0, stubFolder_1, stubFolder_3];
+	let allFolders;
 
 	let stubHierarchy: FolderHierarchy;
 	let driveKey: DriveKey;
 	let fileKey: FileKey;
 
 	before(async () => {
+		// Root folder
+		stubRootFolder = await stubPrivateFolder({
+			folderId: stubEntityIDRoot,
+			parentFolderId: new RootFolderID(),
+			folderName: 'The drive name'
+		});
+		// Depth 1
+		stubParentFolder = await stubPrivateFolder({
+			folderId: stubEntityIDParent,
+			parentFolderId: stubEntityIDRoot,
+			folderName: 'Funny stuff'
+		});
+		// Depth 2
+		stubFolder_0 = await stubPrivateFolder({
+			folderId: stubEntityID,
+			parentFolderId: stubEntityIDParent,
+			folderName: 'Funny folder #1'
+		});
+		stubFolder_1 = await stubPrivateFolder({
+			folderId: stubEntityIDAlt,
+			parentFolderId: stubEntityIDParent,
+			folderName: 'Funny folder #2'
+		});
+		// Depth 3
+		stubFolder_3 = await stubPrivateFolder({
+			folderId: stubEntityIDGrandchild,
+			parentFolderId: stubEntityID,
+			folderName: 'The funniest folder'
+		});
+		stubFile = await stubPrivateFile({
+			fileId: stubEntityIDAltTwo,
+			fileName: 'jokes.pdf'
+		});
+		allFolders = [stubRootFolder, stubParentFolder, stubFolder_0, stubFolder_1, stubFolder_3];
+
 		stubHierarchy = FolderHierarchy.newFromEntities(allFolders);
 		driveKey = await getStubDriveKey();
 		fileKey = stubFile.fileKey;
 	});
 
 	it('Built folders will have the extra driveKey attribute set', async () => {
-		const folderWithKeys = privateEntityWithPathsFactory(stubFolder_0, stubHierarchy, driveKey);
-		expect(folderWithKeys.driveKey).to.equal(urlEncodeHashKey(driveKey));
+		const folderWithKeys = privateEntityWithPathsFactory(stubFolder_0, stubHierarchy);
+		expect(`${folderWithKeys.driveKey}`).to.equal(`${driveKey}`);
 		expect((folderWithKeys as ArFSPrivateFileWithPaths).fileKey).to.be.undefined;
 	});
 
 	it('Built files will have the driveKey and fileKey attributes set', async () => {
-		const fileWithKeys = privateEntityWithPathsFactory(
-			stubFile,
-			stubHierarchy,
-			driveKey
-		) as ArFSPrivateFileWithPaths;
-		expect(fileWithKeys.driveKey).to.equal(urlEncodeHashKey(driveKey));
-		expect(fileWithKeys.fileKey).to.equal(urlEncodeHashKey(fileKey));
+		const fileWithKeys = privateEntityWithPathsFactory(stubFile, stubHierarchy) as ArFSPrivateFileWithPaths;
+		expect(`${fileWithKeys.driveKey}`).to.equal(`${driveKey}`);
+		expect(`${fileWithKeys.fileKey}`).to.equal(`${fileKey}`);
 	});
 
 	it('Folders built with the Keyless factory will not expose its keys', async () => {
-		const fileWithKeys = privateEntityWithPathsKeylessFactory(
-			stubFile,
-			stubHierarchy,
-			driveKey
-		) as ArFSPrivateFileWithPaths;
+		const fileWithKeys = privateEntityWithPathsKeylessFactory(stubFile, stubHierarchy) as ArFSPrivateFileWithPaths;
 		expect(fileWithKeys.driveKey).to.be.undefined;
 		expect(fileWithKeys.fileKey).to.be.undefined;
 	});
 
 	it('Files built with the Keyless factory will not expose its keys', async () => {
-		const fileWithKeys = privateEntityWithPathsKeylessFactory(
-			stubFile,
-			stubHierarchy,
-			driveKey
-		) as ArFSPrivateFileWithPaths;
+		const fileWithKeys = privateEntityWithPathsKeylessFactory(stubFile, stubHierarchy) as ArFSPrivateFileWithPaths;
 		expect(fileWithKeys.driveKey).to.be.undefined;
 		expect(fileWithKeys.fileKey).to.be.undefined;
 	});
