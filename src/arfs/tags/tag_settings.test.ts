99face06147fe187bcb0135396bea2af3c720aac
Derek Sonnenberg <derek@ardrive.io>
2022-05-12 11:18:31 -0500

    feat(custom metadata): Add custom metadata to ArFSTagSettings class

---
+++ src/arfs/tags/tag_settings.test.ts
@@ -0,0 +1,51 @@
+import { expect } from 'chai';
+import { ArFSTagSettings } from './tag_settings';
+
+describe('ArFSTagSettings class', () => {
+	const arFSTagSettings = new ArFSTagSettings({
+		appName: 'Tag-Builder-Test',
+		appVersion: '1.2',
+		arFSVersion: '0.101'
+	});
+
+	it('returns the expected baseline arfs tags', () => {
+		expect(arFSTagSettings.baseArFSTags).to.deep.equal([
+			{ name: 'App-Name', value: 'Tag-Builder-Test' },
+			{ name: 'App-Version', value: '1.2' },
+			{ name: 'ArFS', value: '0.101' }
+		]);
+	});
+
+	it('returns the expected base bundle tags', () => {
+		expect(arFSTagSettings.baseBundleTags).to.deep.equal([
+			{ name: 'App-Name', value: 'Tag-Builder-Test' },
+			{ name: 'App-Version', value: '1.2' },
+			{ name: 'Bundle-Format', value: 'binary' },
+			{ name: 'Bundle-Version', value: '2.0.0' }
+		]);
+	});
+
+	it('returns the expected default tip tags', () => {
+		expect(arFSTagSettings.getTipTags()).to.deep.equal([{ name: 'Tip-Type', value: 'data upload' }]);
+	});
+
+	it('returns the expected tip tags with app tags', () => {
+		expect(arFSTagSettings.getTipTagsWithAppTags()).to.deep.equal([
+			{ name: 'App-Name', value: 'Tag-Builder-Test' },
+			{ name: 'App-Version', value: '1.2' },
+			{ name: 'Tip-Type', value: 'data upload' }
+		]);
+	});
+
+	it('returns the expected file data item tags', () => {
+		console.log(
+			"arFSTagSettings.getFileDataTags(false, 'application/json')",
+			arFSTagSettings.getFileDataItemTags(false, 'application/json')
+		);
+		expect(arFSTagSettings.getFileDataItemTags(false, 'application/json')).to.deep.equal([
+			{ name: 'App-Name', value: 'Tag-Builder-Test' },
+			{ name: 'App-Version', value: '1.2' },
+			{ name: 'Content-Type', value: 'application/json' }
+		]);
+	});
+});
---
+++ src/arfs/tags/tag_settings.ts
@@ -0,0 +1,88 @@
+import { GQLTagInterface, FeeMultiple, TipType, DataContentType } from '../../types';
+import {
+	DEFAULT_APP_NAME,
+	DEFAULT_APP_VERSION,
+	CURRENT_ARFS_VERSION,
+	privateOctetContentTypeTag,
+	privateCipherTag,
+	fakePrivateCipherIVTag
+} from '../../utils/constants';
+
+interface ArFSTagSettingsParams {
+	appName?: string;
+	appVersion?: string;
+	arFSVersion?: string;
+	customTags?: GQLTagInterface[];
+}
+
+export class ArFSTagSettings {
+	private readonly appName: string;
+	private readonly appVersion: string;
+	private readonly arFSVersion: string;
+	private readonly customTags: GQLTagInterface[];
+
+	constructor({
+		appName = DEFAULT_APP_NAME,
+		appVersion = DEFAULT_APP_VERSION,
+		arFSVersion = CURRENT_ARFS_VERSION,
+		customTags = []
+	}: ArFSTagSettingsParams) {
+		this.appName = appName;
+		this.appVersion = appVersion;
+		this.arFSVersion = arFSVersion;
+		this.customTags = customTags;
+	}
+
+	public get baseAppTags(): GQLTagInterface[] {
+		return [
+			{ name: 'App-Name', value: this.appName },
+			{ name: 'App-Version', value: this.appVersion }
+		];
+	}
+
+	public getBoostTags(feeMultiple: FeeMultiple): GQLTagInterface[] {
+		return [{ name: 'Boost', value: feeMultiple.toString() }];
+	}
+
+	public getTipTags(tipType: TipType = 'data upload'): GQLTagInterface[] {
+		return [{ name: 'Tip-Type', value: tipType }];
+	}
+
+	public getCustomTags(): GQLTagInterface[] {
+		return this.customTags;
+	}
+
+	public get baseArFSTags(): GQLTagInterface[] {
+		return [...this.baseAppTags, { name: 'ArFS', value: this.arFSVersion }];
+	}
+
+	public get baseBundleTags(): GQLTagInterface[] {
+		return [
+			...this.baseAppTags,
+			{ name: 'Bundle-Format', value: 'binary' },
+			{ name: 'Bundle-Version', value: '2.0.0' }
+		];
+	}
+
+	/** @deprecated Used for the deprecated flow of sending a separate community tip tx */
+	public getTipTagsWithAppTags(): GQLTagInterface[] {
+		return [...this.baseAppTags, ...this.getTipTags()];
+	}
+
+	/**
+	 * Used for estimating byte count of data items to bypass storing the Buffer from ArFSFileDataPrototype
+	 *
+	 * TODO: Don't use the file data Buffer in ArFSFileDataPrototype so it can be used in estimation without memory concerns
+	 */
+	public getFileDataItemTags(isPrivate: boolean, dataContentType: DataContentType): GQLTagInterface[] {
+		const tags = this.baseAppTags;
+
+		tags.push(
+			...(isPrivate
+				? [privateOctetContentTypeTag, privateCipherTag, fakePrivateCipherIVTag]
+				: [{ name: 'Content-Type', value: dataContentType }])
+		);
+
+		return tags;
+	}
+}
