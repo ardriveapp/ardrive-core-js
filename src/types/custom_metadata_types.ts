00718789d71a3f480d5ec8d196b1bddf0e7fce56
Derek Sonnenberg <derek@ardrive.io>
2022-07-05 11:13:47 -0500

    feat(custom metadata): Add type-guard functions for sanitizing user input PE-1530

--- src/types/custom_metadata_types.ts
+++ src/types/custom_metadata_types.ts
@@ -1,5 +1,31 @@
 export type CustomMetaDataTagInterface = Record<string, string | string[]>;
 
+export function isCustomMetaDataTagInterface(tags: unknown): tags is CustomMetaDataTagInterface {
+	if (typeof tags !== 'object' || tags === null) {
+		return false;
+	}
+
+	for (const value of Object.values(tags)) {
+		if (typeof value === 'string') {
+			continue;
+		}
+
+		if (!Array.isArray(value)) {
+			return false;
+		}
+
+		if (value.length > 1) {
+			for (const item of value) {
+				if (typeof item !== 'string') {
+					return false;
+				}
+			}
+		}
+	}
+
+	return true;
+}
+
 export interface CustomMetaData {
 	tagsOnFileMetaDataJson?: CustomMetaDataTagInterface;
 	tagsOnFileMetaDataGql?: CustomMetaDataTagInterface;
@@ -12,4 +38,20 @@ export interface CustomMetaData {
 
 	// tagsOnDriveMetaDataJson?: CustomMetaDataTagInterface;
 	// tagsOnDriveMetaDataGql?: CustomMetaDataTagInterface;
+}
+
+export function isCustomMetaData(tags: unknown): tags is CustomMetaData {
+	if (typeof tags !== 'object' || tags === null) {
+		return false;
+	}
+
+	for (const [key, val] of Object.entries(tags)) {
+		if (key !== 'tagsOnFileMetaDataJson' && key !== 'tagsOnFileMetaDataGql') {
+			return false;
+		}
+		if (!isCustomMetaDataTagInterface(val)) {
+			return false;
+		}
+	}
+	return true;
 }
