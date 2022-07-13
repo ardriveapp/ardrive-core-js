080651ca3197157a571270417c05beb2bec3df6d
Derek Sonnenberg <derek@ardrive.io>
2022-07-13 11:07:47 -0500

    refactor(custom metadata): Handle assertions and error messages on Core layer PE-1530

--- src/types/custom_metadata_types.ts
+++ src/types/custom_metadata_types.ts
@@ -1,3 +1,14 @@
+const invalidSchemaErrorMessage = `Invalid custom metadata schema. Please submit a valid JSON object with an example shape of `;
+
+const customMetaDataTagInterfaceShapeOne = '{ "TAG_NAME": "TAG_VALUE" }';
+const customMetaDataTagInterfaceShapeTwo = '{ "TAG_NAME": ["VAL 1", "VAL 2" ]}';
+
+export const invalidCustomMetaDataTagInterfaceErrorMessage = `${invalidSchemaErrorMessage}${customMetaDataTagInterfaceShapeOne} or ${customMetaDataTagInterfaceShapeTwo}`;
+
+const customMetaDataShape = `{ metaDataJson?: ${customMetaDataTagInterfaceShapeOne}, metaDataGql?: ${customMetaDataTagInterfaceShapeTwo} }`;
+
+export const invalidCustomMetaDataErrorMessage = `s${invalidSchemaErrorMessage}${customMetaDataShape}`;
+
 export type CustomMetaDataTagInterface = Record<string, string | string[]>;
 
 export interface CustomMetaData {
@@ -19,6 +30,7 @@ export function isCustomMetaDataTagInterface(tags: unknown): tags is CustomMetaD
 
 	for (const value of Object.values(tags)) {
 		if (typeof value === 'string') {
+			assertCharacterLength(value);
 			continue;
 		}
 
@@ -31,11 +43,18 @@ export function isCustomMetaDataTagInterface(tags: unknown): tags is CustomMetaD
 				if (typeof item !== 'string') {
 					return false;
 				}
+				assertCharacterLength(item);
 			}
 		}
 	}
 
 	return true;
+}
+
+function assertCharacterLength(value: string): void {
+	if (value.length === 0) {
+		throw Error('Metadata string must be at least one character!');
+	}
 }
 
 export function isCustomMetaData(tags: unknown): tags is CustomMetaData {
@@ -50,6 +69,22 @@ export function isCustomMetaData(tags: unknown): tags is CustomMetaData {
 		if (!isCustomMetaDataTagInterface(val)) {
 			return false;
 		}
+	}
+	return true;
+}
+
+export function assertCustomMetaDataTagInterface(tags: unknown): tags is CustomMetaDataTagInterface {
+	if (!isCustomMetaDataTagInterface(tags)) {
+		console.log('invalidCustomMetaDataTagInterfaceErrorMessage', invalidCustomMetaDataTagInterfaceErrorMessage);
+		throw Error(invalidCustomMetaDataTagInterfaceErrorMessage);
+	}
+	return true;
+}
+
+export function assertCustomMetaData(tags: unknown): tags is CustomMetaData {
+	if (!isCustomMetaData(tags)) {
+		console.log('invalidCustomMetaDataErrorMessage', invalidCustomMetaDataErrorMessage);
+		throw Error(invalidCustomMetaDataErrorMessage);
 	}
 	return true;
 }
