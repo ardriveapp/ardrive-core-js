1c034461ae97dbf5e173392b07851d76d79a0011
Derek Sonnenberg <derek@ardrive.io>
2022-07-28 15:49:57 -0500

    test(custom metadata): Use raw expected tag names PE-1972

--- src/types/custom_metadata_type.test.ts
+++ src/types/custom_metadata_type.test.ts
@@ -1,10 +1,30 @@
 import { expect } from 'chai';
-import { gqlTagNameArray } from '../utils/constants';
 import { isCustomMetaDataGqlTags } from './custom_metadata_types';
 
 describe('isCustomMetaDataGqlTags type guard function', () => {
-	describe('returns false if provided tag name collides with any protected ArFS GQL tag name', () => {
-		for (const tagName of gqlTagNameArray) {
+	it('returns false if provided tag name collides with any protected ArFS GQL tag name', () => {
+		const tagNames = [
+			'App-Name',
+			'App-Version',
+			'ArFS',
+			'Tip-Type',
+			'Content-Type',
+			'Boost',
+			'Bundle-Format',
+			'Bundle-Version',
+			'Entity-Type',
+			'Unix-Time',
+			'Drive-Id',
+			'Folder-Id',
+			'File-Id',
+			'Parent-Folder-Id',
+			'Drive-Privacy',
+			'Cipher',
+			'Cipher-IV',
+			'Drive-Auth-Mode'
+		];
+
+		for (const tagName of tagNames) {
 			expect(isCustomMetaDataGqlTags({ [tagName]: 'Val' })).to.be.false;
 		}
 	});
